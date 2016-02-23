087887d43f315e53b60d25b740b5ecd8d3edab8e
Rui Yang <ryang.sei@gmail.com>
2016-02-23 23:09:39 +1100

    add more tables

--- resources/migrations/201601242146000-create-user-table.up.sql
+++ resources/migrations/201601242146000-create-user-table.up.sql
@@ -1,1 +1,13 @@
-CREATE TABLE IF NOT EXISTS User(id Identity, first_name varchar(50), last_name varchar(50), email varchar(50), password varchar(200));
+CREATE TABLE IF NOT EXISTS User(id IDENTITY, email VARCHAR(50) UNIQUE, first_name VARCHAR(50), last_name VARCHAR(50), password VARCHAR(200));
+
+CREATE TABLE IF NOT EXISTS Business(id IDENTITY, user_id INT, business_name VARCHAR(50));
+
+CREATE TABLE IF NOT EXISTS Item(id IDENTITY, business_id INT NOT NULL, code VARCHAR(50), description VARCHAR(250), price DECIMAL(10,2), thumbnail BINARY, create_date TIMESTAMP, last_modify_date TIMESTAMP,FOREIGN KEY (business_id) REFERENCES Business(id));
+
+CREATE TABLE IF NOT EXISTS Image(id IDENTITY, item_id INT NOT NULL, image BINARY, FOREIGN KEY (ITEM_ID) references Item(id));
+
+CREATE TABLE IF NOT EXISTS Client(id IDENTITY, wei_id varchar(250), business_id INT, name varchar(50), create_date TIMESTAMP, last_modify_date TIMESTAMP, CONSTRAINT uc_id_user_id UNIQUE (id, business_id), FOREIGN KEY (business_id) REFERENCES Business(id));
+
+CREATE TABLE IF NOT EXISTS ClientOrder(id IDENTITY, client_id INT, business_id INT, create_date TIMESTAMP, last_modify_date TIMESTAMP, FOREIGN KEY (client_id) REFERENCES Client(id), FOREIGN KEY (business_id) REFERENCES Business(id));
+
+CREATE TABLE IF NOT EXISTS OrderItem (id IDENTITY, order_id INT, item_id INT, quantity INT, price DECIMAL(10,2), create_date TIMESTAMP, last_modify_date TIMESTAMP, FOREIGN KEY (item_id) REFERENCES Item(id), FOREIGN KEY (order_id) REFERENCES ClientOrder(id));
