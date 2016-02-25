0857748cc72dd9fb0f1d9a1a038f6d3141977fb3
Rui Yang <ryang.sei@gmail.com>
2016-02-25 11:22:38 +1100

    add stock to item table

--- resources/migrations/201601242146000-create-user-table.up.sql
+++ resources/migrations/201601242146000-create-user-table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS User(id IDENTITY, email VARCHAR(50) UNIQUE, first_nam
 
 CREATE TABLE IF NOT EXISTS Business(id IDENTITY, user_id INT, business_name VARCHAR(50));
 
-CREATE TABLE IF NOT EXISTS Item(id IDENTITY, business_id INT NOT NULL, code VARCHAR(50), description VARCHAR(250), price DECIMAL(10,2), thumbnail BINARY, create_date TIMESTAMP, last_modify_date TIMESTAMP,FOREIGN KEY (business_id) REFERENCES Business(id));
+CREATE TABLE IF NOT EXISTS Item(id IDENTITY, business_id INT NOT NULL, code VARCHAR(50), description VARCHAR(250), price DECIMAL(10,2), stock INT NULL DEFAULT(0), thumbnail BINARY, create_date TIMESTAMP, last_modify_date TIMESTAMP,FOREIGN KEY (business_id) REFERENCES Business(id));
 
 CREATE TABLE IF NOT EXISTS Image(id IDENTITY, item_id INT NOT NULL, image BINARY, FOREIGN KEY (ITEM_ID) references Item(id));
 
